dc4dd75e381f8fcb079cec89d3244fa439f5aa0e
Andrew Ho <ho@thundermac.local>
2015-11-01 00:15:28 -0700

    styling additions for wedding page (rough draft)

--- src/views/wedding/wedding.js
+++ src/views/wedding/wedding.js
@@ -10,25 +10,65 @@
 	];
 	function WeddingController($scope) {
 
-		$scope.category = {
-			img: 'assets/imgs/story/el-farallon.jpg',
+		$scope.categories = {
+			venue: {
+				name: 'Ceremony & Reception',
+				img: 'assets/imgs/wedding/venue.jpg',
+				places: [
+					{
+						name: 'Gerry Ranch',
+						address: {
+							address1: '9015 Rosita Rd.',
+							address2: '',
+							city: 'Camarillo',
+							state: 'CA',
+							postalCode: '93012',
+							mapLink: 'www.google.com/maps/place/Gerry+Ranch/@34.2421004,-118.9479015,17z/data=!3m1!4b1!4m2!3m1!1s0x80e8314949bb9183:0xd6cb1cd1d06ec367'
+						},
+					}
+				]
+			},
+			schedule: {
+				name: 'Event Schedule',
+				img: 'assets/imgs/wedding/schedule.jpg',
+			},
+			attire: {
+				name: 'Attire',
+				img: 'assets/imgs/wedding/attire.jpg',
+			},
+			hotels: {
+				name: 'Hotel Accommodations',
+				img: 'assets/imgs/wedding/hotels.jpg',
+				places: [
+					{
+						name: 'Sheraton Inn',
+						address: {
+							address1: '1234 blah',
+							address2: '',
+							city: 'Tustin',
+							state: 'CA',
+							country: 'USA'
+						},
+						phone: '(555) 555-5555',
+						description: 'Japanese deliciousness',
+					},
+					{
+						name: 'Embassy Suites',
+						website: 'www.google.com',
+						address: {
+							address1: '1234 blah',
+							address2: '',
+							city: 'Tustin',
+							state: 'CA',
+							country: 'USA'
+						},
+						phone: '(555) 555-5555',
+						description: 'Japanese deliciousness',
+					}
+				]
+			},
 		};
 
-		$scope.places = [
-			{
-				name: 'Honda-ya',
-				website: 'www.google.com',
-				address: {
-					address1: '1234 blah',
-					address2: '',
-					city: 'Tustin',
-					state: 'CA',
-					country: 'USA'
-				},
-				phone: '(555) 555-5555',
-				description: 'Japanese deliciousness',
-			}
-		];
 	}
 
 })();
