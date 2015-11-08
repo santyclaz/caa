d15dc39ad5f4149970c32a59c34f50bfc89cc54d
Andrew Ho <ho@thundermac.local>
2015-11-08 12:28:48 -0800

    styling changes and adding additional place information

--- src/views/wedding/wedding.js
+++ src/views/wedding/wedding.js
@@ -23,8 +23,8 @@
 							city: 'Camarillo',
 							state: 'CA',
 							postalCode: '93012',
-							mapLink: 'www.google.com/maps/place/Gerry+Ranch/@34.2421004,-118.9479015,17z/data=!3m1!4b1!4m2!3m1!1s0x80e8314949bb9183:0xd6cb1cd1d06ec367'
 						},
+						mapLink: 'https://www.google.com/maps/place/Gerry+Ranch/@34.2421004,-118.9479015,17z/data=!3m1!4b1!4m2!3m1!1s0x80e8314949bb9183:0xd6cb1cd1d06ec367'
 					}
 				]
 			},
@@ -41,29 +41,43 @@
 				img: 'assets/imgs/wedding/hotels.jpg',
 				places: [
 					{
-						name: 'Sheraton Inn',
+						name: 'Hyatt Westlake Plaza',
 						address: {
-							address1: '1234 blah',
+							address1: '8080 S Westlake Blvd.',
 							address2: '',
-							city: 'Tustin',
+							city: 'Westlake Village',
 							state: 'CA',
-							country: 'USA'
+							postalCode: '91361'
 						},
-						phone: '(555) 555-5555',
-						description: 'Japanese deliciousness',
+						phone: '(805) 557-1234',
+						website: {
+							url: 'http://www.hyattwestlake.com',
+							label: 'www.hyattwestlake.com'
+						},
+						// TODO: see if this can be refactored
+						reservation: {
+							url: 'https://resweb.passkey.com/go/leungho',
+							price: '$174',
+						}
 					},
 					{
-						name: 'Embassy Suites',
-						website: 'www.google.com',
+						name: 'Hampton Inn & Suites',
 						address: {
-							address1: '1234 blah',
+							address1: '30255 Agoura Road',
 							address2: '',
-							city: 'Tustin',
+							city: 'Agoura Hills',
 							state: 'CA',
-							country: 'USA'
+							postalCode: '91301'
 						},
-						phone: '(555) 555-5555',
-						description: 'Japanese deliciousness',
+						phone: '(818) 597-0333',
+						website: {
+							url: 'http://hamptoninn3.hilton.com/en/hotels/california/hampton-inn-and-suites-agoura-hills-AGOCAHX/index.html',
+							label: 'hamptoninn3.hilton.com'
+						},
+						reservation: {
+							price: '$136',
+							fixedPrice: true
+						}
 					}
 				]
 			},
